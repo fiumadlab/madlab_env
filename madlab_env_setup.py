b29a4fcb47723deee17174073e5e53bb725caa07
Adam Kimbler <akimbler@fiu.edu>
2017-08-03 20:38:04 -0400

    Update madlab_env_setup.py

--- madlab_env_setup.py
+++ madlab_env_setup.py
@@ -2,11 +2,8 @@
 import os
 import time
 
-<<<<<<< HEAD
-FILES = ['.bashrc', '.bash_profile','.env','.projects', '.nodeload']
-=======
+
 FILES = ['.bashrc', '.bash_profile', '.projects', '.env', '.nodeload']
->>>>>>> 80c81d62287536f3011d4ea7c58a1ed912c13709
 
 for f in FILES:
     if os.path.exists(os.path.expanduser('~/{0}'.format(f))):
@@ -15,11 +12,7 @@ for f in FILES:
         cmd = 'mv ~/{0} {1}'.format(f, newname)
         print(cmd)
         os.system(cmd)
-<<<<<<< HEAD
     cmd = 'ln -s {0} ~/'.format(os.path.abspath(f))
-=======
-    cmd = 'ln -s {} ~/'.format(os.path.abspath(f))
->>>>>>> 80c81d62287536f3011d4ea7c58a1ed912c13709
     print(cmd)
     os.system(cmd)
 
