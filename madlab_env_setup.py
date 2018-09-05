9c81350cb5568740fe0342c59db04f8be7bbc130
adamkimbler <adamkmblr@gmail.com>
2018-09-05 12:44:12 -0400

    Finished Debug of New Env Setup Script

--- madlab_env_setup.py
+++ madlab_env_setup.py
@@ -2,20 +2,20 @@ import os
 import time
 from subprocess import Popen
 
-files = ['.bashrc', '.bash_profile', '.projects', '.env', '.nodeload']
+files = ['.bashrc', '.bash_profile', '.projects', '.nodeload']
 
 for file_name in files:
-    sym_path = os.path.dirname(os.path.realpath(__file__)) + file_name
+    sym_path = os.path.dirname(os.path.realpath(__file__)) + '/' + file_name
     print(sym_path)
-    if os.path.exists(os.path.expanduser('~/{0}'.format(file_name)):
+    if os.path.exists(os.path.expanduser('~/{0}'.format(file_name))):
         now = ''.join(time.ctime().split(' ')).replace(':', '')
         newname = '~/%s_%s' % (file_name, now)
         cmd = 'mv ~/%s %s' % (file_name, newname)
         print(cmd)
-        os.system(cmd)
+        Popen(cmd, shell=True).wait()
     cmd = 'ln -s %s ~/' % os.path.abspath(sym_path)
     print(cmd)
-    Popen(cmd, shell=True)
+    Popen(cmd, shell=True).wait()
 
 if not os.path.exists(os.path.expanduser('~/.custom_env')):
-    Popen('touch ~/.custom_env', shell=True)
+    Popen('touch ~/.custom_env', shell=True).wait()
